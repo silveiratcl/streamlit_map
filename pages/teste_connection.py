f53f10a6baa540b6ab7a573b813fa660762e9692
silveiratcl <silveira.tcl@gmail.com>
2025-05-07 13:38:33 -0300

    teste just conn

--- pages/teste_connection.py
+++ pages/teste_connection.py
@@ -44,14 +44,23 @@ def init_connection():
         engine = create_engine(connection_string)
         
         # Test the connection
-        conn = engine.connect()
-        conn.execute(text("SELECT 1"))  # Simple test query
-       # st.success("Connected to database successfully!")
-        return conn
-        
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        return engine  # Return engine instead of connection
     except Exception as e:
         st.error(f"Database connection error: {e}")
-        raise  # Re-raise the exception to stop execution
+        raise
+
+
+        # Test the connection
+        #conn = engine.connect()
+        #conn.execute(text("SELECT 1"))  # Simple test query
+       # st.success("Connected to database successfully!")
+        #return conn
+        
+    #except Exception as e:
+     #   st.error(f"Database connection error: {e}")
+      #  raise  # Re-raise the exception to stop execution
 
 # Initialize the connection
 conn = init_connection()
@@ -61,28 +70,28 @@ conn = init_connection()
 @st.cache_data
 def get_management_data(ttl=300):
     query = "SELECT management_id, management_coords, observer, managed_mass_kg, date FROM data_coralsol_management"
-    df = pd.read_sql(query, conn.engine)
+    df = pd.read_sql(query, conn)
     df.columns = map(str.lower, df.columns)
     return df
 
 @st.cache_data
 def get_locality_data(ttl=300):
     query = "SELECT locality_id, coords_local, name, date FROM data_coralsol_locality"
-    df = pd.read_sql(query, conn.engine)
+    df = pd.read_sql(query, conn)
     df.columns = map(str.lower, df.columns)
     return df
 
 @st.cache_data
 def get_occ_data(ttl=300):
     query = "SELECT Occurrence_id, Spot_coords, Date, Depth, Superficie_photo FROM data_coralsol_occurrence WHERE Superficie_photo IS NOT NULL LIMIT 10"
-    df = pd.read_sql(query, conn.engine)
+    df = pd.read_sql(query, conn)
     df.columns = map(str.lower, df.columns)
     return df
 
 @st.cache_data
 def get_dafor_data(ttl=300):
     query = "SELECT Dafor_id, Locality_id, Dafor_coords, Date, Horizontal_visibility, Bathymetric_zone, Method, Dafor_value FROM data_coralsol_dafor"
-    df = pd.read_sql(query, conn.engine)
+    df = pd.read_sql(query, conn)
     df.columns = map(str.lower, df.columns)
     return df
 
